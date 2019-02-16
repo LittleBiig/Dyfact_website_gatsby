0833fafb7d4d39f2ebbade35a3d9109c57e75be3
LittleBiig <16526995+LittleBiig@users.noreply.github.com>
2019-02-16 11:06:26 +0100

    Site en maintenance

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,8 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <div>
-    <SEO title="Home" keywords={[`dyfact`, `dyfactmusic` ]} />
-    <h1>Site en maintenance</h1>
-    <p>Vous êtes bien sur le site des DJs/Producteurs 'Dyfact'. Le site est actuellement en cours de maintenance.</p>
-    <p>Vous pouvez nous retrouver sur les reseaux sociaux <a href={"https://web.facebook.com/Dyfactmusic"}>Facebook</a>, <a href={"https://www.instagram.com/dyfact/"}>Instagram</a> et <a href="https://www.youtube.com/channel/UC0jIHcMBGwfCz5mCjnngM_g">Youtube</a></p>
-  <div>
-      <p>test GraphQL</p>
-        <HomePage/>
+      <SEO title="Home" keywords={[`dyfact`, `dyfactmusic` ]} />
+      <HomePage />
   </div>
   </div>
 );

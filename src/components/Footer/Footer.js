6044ffb280052b6344a853b9b40ccc4b570a51e7
LittleBiig <16526995+LittleBiig@users.noreply.github.com>
2019-02-24 19:30:41 +0100

    Add Footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 const Footer = () => (
-    <div>hello</div>
+    <footer className="row">
+        <div className="row logo-footer ">
+            <div className="col-md-4 col-md-offset-4 text-center "><img src="src/logos/Logo_Dyfact.png"
+                                                                        alt="" /></div>
+        </div>
+        <div className="row copyrights ">
+            <div className="col-md-4 col-md-offset-4 text-center ">
+                <p>© Dyfact 2018 | Tous droits reservés.</p>
+            </div>
+        </div>
+        <div>
+            <img src="src/logos/Logo_Dyfact_black.png"
+                 style="display:none"
+                 alt="" />
+                <img src="src/logos/logo-colors.png"
+                     style="display:none"
+                     alt="" />
+                    <img src="src/logos/texte_griffures.png"
+                         style="display:none"
+                         alt="" />
+        </div>
+    </footer>
 );
 
 export default Footer;

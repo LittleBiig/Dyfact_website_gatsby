074559e307b27867028d10ec95a47222d7c175dc
LittleBiig <16526995+LittleBiig@users.noreply.github.com>
2020-03-16 23:31:06 +0100

    Add ATWG Cover

--- src/components/Musics/Musics.js
+++ src/components/Musics/Musics.js
@@ -12,6 +12,16 @@ const Music = () => (
             <div className="col-12 offset-xl-2 col-xl-8 justify-content-center">
                 <div className={"row justify-content-center"}>
                     <div className={"offset-2 col-8 offset-md-0 col-md-4 col-xl-3 mx-3 cover"}>
+                        <a href="https://www.youtube.com/watch?v=D0p17posSEM"
+                           target="_blank"
+                           rel="noopener noreferrer"
+                        >
+                            <img src="https://i.scdn.co/image/ab67616d0000b273242233c0738d7cc5f1f191f5"
+                                 alt="cover ATWG"
+                            />
+                        </a>
+                    </div>
+                    <div className={"offset-2 col-8 offset-md-0 col-md-4 col-xl-3 mx-3 cover"}>
                         <a href="https://www.youtube.com/watch?v=SShagzemD3k"
                            target="_blank"
                            rel="noopener noreferrer"
